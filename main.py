f215c3de722e389c22b55120da6a22aca24ae327
korotkov_egor <korotkovegor10@gmail.com>
2024-09-28 21:40:37 +0300

    add response to text message

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 import telebot
 from dotenv import dotenv_values
@@ -38,8 +39,7 @@ def help(message):
 
 <u>/photo</u>                    показать мое фото
 
-<u>/github</u>   <u>/site</u>        показать мой github
-<u>/website</u>   <u>/git</u>''',
+<u>/github</u>   <u>/site</u>        показать мой github''',
 parse_mode='html'
 )
 
@@ -79,7 +79,7 @@ def tell_about_music(callback):
 
 
 @bot.callback_query_handler(func=lambda callback: True)
-def callback_message(callback):
+def callback_about_me(callback):
         if callback.data == 'програмирование':
                 tell_about_programming(callback)
         elif callback.data == 'музыка':
@@ -89,10 +89,50 @@ def callback_message(callback):
                 tell_about_music(callback)
 
 
-# responds to photo
-@bot.message_handler(content_types=['photo'])
-def responds_to_photo(message):
-        bot.reply_to(message, '<b>Зачем ты мне это отправил?</b>', parse_mode='html')
+# responds to photo, video and audio
+@bot.message_handler(content_types=['photo', 'video', 'voice'])
+def error_type(message):
+        bot.reply_to(message, 'Бот не может считывать информацию в таком виде')
+
+
+# responds to text file
+@bot.message_handler(content_types=['document'])
+def responds_to_file(message):
+        file_info = bot.get_file(message.document.file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+
+        with open('./user_file.txt', 'wb') as new_file:
+                new_file.write(downloaded_file)
+        
+        if os.path.getsize('./user_file.txt') > 10 ** 6:
+                bot.reply_to(message, 'Файл слишком большой')
+                return
+
+        file = open('./user_file.txt', 'r+')
+
+        user_commands = []
+        string = file.readline().strip()
+        while string:
+                user_commands += string.split()
+                string = file.readline().strip()
+        
+        correct_commands = ['/start', '/hello', '/help', '/about_me', '/photo', '/github', '/git']
+        correct_user_commands = []
+        for command in user_commands:
+                for correct_command in correct_commands:
+                        usr_comm = command.strip().lower()
+                        if usr_comm == correct_command.strip() and usr_comm not in correct_user_commands:
+                                correct_user_commands.append(correct_command)
+        
+        correct_user_commands = '\n'.join(correct_user_commands)
+        if correct_user_commands:
+                bot.reply_to(message,
+f'''Может быть вы хотели ввести эти команды(у):
+{correct_user_commands}''')
+        else:
+                bot.reply_to(message, 'Данный файл не содержит команд')
+        file.truncate(0)
+        file.close()
 
 
 # show photo
@@ -115,7 +155,7 @@ def open_github(message):
         bot.send_message(message.chat.id, 'Вот мой git', reply_markup=markup)
 
 
-@bot.message_handler(commands=['github', 'site', 'website', 'git'])
+@bot.message_handler(commands=['github', 'git'])
 def transfer_to_site(message):
         open_github(message)
 
