dc038b41566656545346639c1b0c57956461ce1f
korotkov_egor <korotkovegor10@gmail.com>
2024-10-09 19:56:36 +0300

    update text_commands

--- bot/handlers/text_commands.py
+++ bot/handlers/text_commands.py
@@ -1,4 +1,13 @@
-from bot.handlers import about_me, say_hello, open_github, show_all_commands, give_photo
+from bot.handlers import (
+    about_me,
+    say_hello,
+    open_github,
+    show_all_commands,
+    give_photo,
+    calculator,
+    rock_scissors_paper,
+)
+
 
 def handler(message, bot):
     match message.text.lower():
@@ -11,4 +20,9 @@ def handler(message, bot):
         case "фото":
             give_photo.handler(message, bot)
         case "о тебе":
-            about_me.autobiography(message)
+            about_me.handler(message, bot)
+        case "калькулятор":
+            calc = calculator.Calculator()
+            calc.handler(message, bot)
+        case "камень, ножницы, бумага":
+            rock_scissors_paper.handler(message, bot)
