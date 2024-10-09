893fb1293b51cb1435237864575ba4ae65a5877c
korotkov_egor <korotkovegor10@gmail.com>
2024-10-09 20:01:55 +0300

    update help

--- bot/handlers/show_all_commands.py
+++ bot/handlers/show_all_commands.py
@@ -1,11 +1,17 @@
 def handler(message, bot):
     bot.send_message(
         message.chat.id,
-        """<b>команды:</b>              <b>функции:</b>
+        '''<b>команды:</b>              <b>функции:</b>
 <u>/start</u>   <u>/hello</u>          начать диалог
 
 <u>/photo</u>                    показать мое фото
 
-<u>/github</u>   <u>/site</u>        показать мой github""",
+<u>/github</u>   <u>/site</u>        показать мой github
+
+<u>/about_me</u>        рассказать обо мне
+
+<u>/calculator</u>        показать калькулятор
+
+<u>/rock_scissors_paper</u>        показать игру "камень, ножницы, бумага"''',
         parse_mode="html",
     )
