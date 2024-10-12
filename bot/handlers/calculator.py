04d624e61bfb180e5cac6c9b74cb3aa356bf61e1
korotkov_egor <korotkovegor10@gmail.com>
2024-10-12 17:57:26 +0300

    update quiz

--- bot/handlers/calculator.py
+++ bot/handlers/calculator.py
@@ -1,6 +1,14 @@
 import telebot
 from dataclasses import dataclass
 
+# dependencies = {
+#     'digs': ['signs', '.', '%', '+/-', 'DEL'],
+#     'signs': ['digs', 'DEL'],
+#     '.': ['digs', 'DEL'],
+#     '%': ['digs', 'signs','DEL']
+
+# }
+
 
 @dataclass
 class State:
@@ -13,34 +21,30 @@ class Calculator:
         self._users = {}
 
     def handler(self, message, bot):
-        mrk = telebot.types.InlineKeyboardMarkup()
-
-        dig_btn0 = telebot.types.InlineKeyboardButton("0", callback_data="cal/0")
-        dig_btn1 = telebot.types.InlineKeyboardButton("1", callback_data="cal/1")
-        dig_btn2 = telebot.types.InlineKeyboardButton("2", callback_data="cal/2")
-        dig_btn3 = telebot.types.InlineKeyboardButton("3", callback_data="cal/3")
-        dig_btn4 = telebot.types.InlineKeyboardButton("4", callback_data="cal/4")
-        dig_btn5 = telebot.types.InlineKeyboardButton("5", callback_data="cal/5")
-        dig_btn6 = telebot.types.InlineKeyboardButton("6", callback_data="cal/6")
-        dig_btn7 = telebot.types.InlineKeyboardButton("7", callback_data="cal/7")
-        dig_btn8 = telebot.types.InlineKeyboardButton("8", callback_data="cal/8")
-        dig_btn9 = telebot.types.InlineKeyboardButton("9", callback_data="cal/9")
-        del_btn = telebot.types.InlineKeyboardButton("DEL", callback_data="cal/delete")
-        sign_change_btn = telebot.types.InlineKeyboardButton("+/-", callback_data="cal/change sign")
-        conv_to_perc_btn = telebot.types.InlineKeyboardButton("%", callback_data="cal/conversion to percentage")
-        div_btn = telebot.types.InlineKeyboardButton("÷", callback_data="cal//")
-        mult_btn = telebot.types.InlineKeyboardButton("*", callback_data="cal/*")
-        sub_btn = telebot.types.InlineKeyboardButton("-", callback_data="cal/-")
-        add_btn = telebot.types.InlineKeyboardButton("+", callback_data="cal/+")
-        eql_btn = telebot.types.InlineKeyboardButton("=", callback_data="cal/=")
-        com_btn = telebot.types.InlineKeyboardButton(".", callback_data="cal/.")
-        bracket_btn = telebot.types.InlineKeyboardButton("()", callback_data="cal/()")
-
-        mrk.row(del_btn, sign_change_btn, conv_to_perc_btn, div_btn)
-        mrk.row(dig_btn7, dig_btn8, dig_btn9, mult_btn)
-        mrk.row(dig_btn4, dig_btn5, dig_btn6, sub_btn)
-        mrk.row(dig_btn1, dig_btn2, dig_btn3, add_btn)
-        mrk.row(dig_btn0, com_btn, bracket_btn, eql_btn)
+        mrk = telebot.util.quick_markup(
+            {
+                "DEL": {"callback_data": "cal/delete"},
+                "+/-": {"callback_data": "cal/change sign"},
+                "%": {"callback_data": "cal/conversion to percentage"},
+                "÷": {"callback_data": "cal//"},
+                "7": {"callback_data": "cal/7"},
+                "8": {"callback_data": "cal/8"},
+                "9": {"callback_data": "cal/9"},
+                "*": {"callback_data": "cal/*"},
+                "4": {"callback_data": "cal/4"},
+                "5": {"callback_data": "cal/5"},
+                "6": {"callback_data": "cal/6"},
+                "-": {"callback_data": "cal/-"},
+                "1": {"callback_data": "cal/1"},
+                "2": {"callback_data": "cal/2"},
+                "3": {"callback_data": "cal/3"},
+                "+": {"callback_data": "cal/+"},
+                "0": {"callback_data": "cal/0"},
+                ".": {"callback_data": "cal/."},
+                "=": {"callback_data": "cal/="},
+            },
+            row_width=4,
+        )
 
         state = self._users.setdefault(message.from_user.id, State(expression="", mrk=mrk))
 
@@ -52,16 +56,6 @@ class Calculator:
     def processing_all_in_expression(self, cmd, expression):
         dig_to_add = processing_nums(cmd)
         expression += dig_to_add
-
-        if cmd == "()":
-            if not expression or expression[-1] == " ":
-                return ""
-            open_bracket_count = expression.count("(")
-            close_bracket_count = expression.count(")")
-            if open_bracket_count == close_bracket_count:
-                expression += "("
-            else:
-                expression += ")"
 
         if not expression:
             return ""
--- bot/handlers/quiz.py
+++ bot/handlers/quiz.py
@@ -1,125 +1,79 @@
 import telebot
 from dataclasses import dataclass
 
+
+@dataclass
+class Question:
+    question: str
+    answers: tuple[str, str, str]
+    correct: int
+
+
+QUESTIONS_AND_ANSWERS = [
+    Question(question="Как меня зовут?", answers=("Егор", "Андрей", "Амид"), correct=1),
+    Question(question="Сколько мне лет?", answers=("13", "14", "15"), correct=2),
+    Question(question="В какой школе я учусь?", answers=("1158", "1488", "2007"), correct=3),
+    Question(question="Какую музыку я люблю?", answers=("Металл", "Пост-панк", "Панк"), correct=2),
+    Question(
+        question="Какими видами спорта я занимался?",
+        answers=("Плававнье и футбол", "Хоккей и футбол", "Скалолазанье и футбол"),
+        correct=3,
+    ),
+    Question(question="На каком языке программирования я пишу?", answers=("Python", "C++", "Java"), correct=1),
+    Question(question="Какой вид одежды я хочу купить?", answers=("Рубашка", "Пальто", "Джинсовка"), correct=2),
+    Question(question="Какой мой любимый цвет?", answers=("Синий", "Красный", "Зеленый"), correct=3),
+    Question(question="Какие аниме я посмотрел?", answers=("Наруто", "Адский рай", "Убийца гоблинов"), correct=3),
+    Question(
+        question="Моя любимая игра? (Есть и другие игры, которые мне тоже очень сильно нравятся)",
+        answers=("Sanabi", "The forest", "Stardew Valley"),
+        correct=1,
+    ),
+]
+
+
 @dataclass
 class State:
     question_number: int
     correct_user_answers_count: int
 
 
+REZULT_END = (
+    "Ты меня совсем не знаешь. Хочешь еще раз попробавать?",
+    "Ты меня  знаешь. Хочешь еще раз попробавать?" "Ты меня немного знаешь. Хочешь еще раз попробавать?",
+    "Ты меня немного знаешь. Хочешь еще раз попробавать?",
+    "Мы с тобой только познакомились. Хочешь еще раз попробавать?",
+    "Я с тобой почти не общаюсь. Хочешь еще раз попробавать?",
+    "Ты меня более мение знаешь. Хочешь еще раз попробавать?",
+    "Ты меня знаешь больше 50%. Хочешь еще раз попробавать?",
+    "Мы переодически общаемся с тобой. Хочешь еще раз попробавать?",
+    "Ты меня знаешь на четверочку. Хочешь еще раз попробавать?",
+    "Ты меня знаешь почти идеально. Хочешь еще раз попробавать?",
+    "Ты мой лучший друг?",
+)
+
+
 class Quiz:
     def __init__(self):
         self._users = {}
 
-    def show_user_result(self, message, bot):
-        state = self._users[message.from_user.id]
+    def show_user_result(self, bot, chat_id, user_id):
+        state = self._users[user_id]
 
         state.question_number = 0
         markup = telebot.types.InlineKeyboardMarkup()
         btn1 = telebot.types.InlineKeyboardButton("Да", callback_data="qzrpl/да")
         btn2 = telebot.types.InlineKeyboardButton("Нет", callback_data="qzrpl/нет")
         markup.row(btn1, btn2)
-        match state.correct_user_answers_count:
-            case 0:
-                state.correct_user_answers_count = 0
-                bot.send_message(
-                    message.chat.id,
-                    "Ты ответил на 0 вопросов из 10 правильно. Ты меня совсем не знаешь. Хочешь еще раз попробавать?",
-                    reply_markup=markup,
-                )
-            case 1:
-                state.correct_user_answers_count = 0
-                bot.send_message(
-                    message.chat.id,
-                    "Ты ответил на 1 вопросов из 10 правильно. Ты меня  знаешь. Хочешь еще раз попробавать?",
-                    reply_markup=markup,
-                )
-            case 2:
-                state.correct_user_answers_count = 0
-                bot.send_message(
-                    message.chat.id,
-                    "Ты ответил на 2 вопросов из 10 правильно. Ты меня немного знаешь. Хочешь еще раз попробавать?",
-                    reply_markup=markup,
-                )
-            case 3:
-                state.correct_user_answers_count = 0
-                bot.send_message(
-                    message.chat.id,
-                    "Ты ответил на 3 вопросов из 10 правильно. Мы с тобой только познакомились. Хочешь еще раз попробавать?",
-                    reply_markup=markup,
-                )
-            case 4:
-                state.correct_user_answers_count = 0
-                bot.send_message(
-                    message.chat.id,
-                    "Ты ответил на 4 вопросов из 10 правильно. Я с тобой почти не общаюсь. Хочешь еще раз попробавать?",
-                    reply_markup=markup,
-                )
-            case 5:
-                state.correct_user_answers_count = 0
-                bot.send_message(
-                    message.chat.id,
-                    "Ты ответил на 5 вопросов из 10 правильно. Ты меня более мение знаешь. Хочешь еще раз попробавать?",
-                    reply_markup=markup,
-                )
-            case 6:
-                state.correct_user_answers_count = 0
-                bot.send_message(
-                    message.chat.id,
-                    "Ты ответил на 6 вопросов из 10 правильно. Ты меня знаешь больше 50%. Хочешь еще раз попробавать?",
-                    reply_markup=markup,
-                )
-            case 7:
-                state.correct_user_answers_count = 0
-                bot.send_message(
-                    message.chat.id,
-                    "Ты ответил на 7 вопросов из 10 правильно. Мы переодически общаемся с тобой. Хочешь еще раз попробавать?",
-                    reply_markup=markup,
-                )
-            case 8:
-                state.correct_user_answers_count = 0
-                bot.send_message(
-                    message.chat.id,
-                    "Ты ответил на 8 вопросов из 10 правильно. Ты меня знаешь на четверочку. Хочешь еще раз попробавать?",
-                    reply_markup=markup,
-                )
-            case 9:
-                state.correct_user_answers_count = 0
-                bot.send_message(
-                    message.chat.id,
-                    "Ты ответил на 9 вопросов из 10 правильно. Ты меня знаешь почти идеально. Хочешь еще раз попробавать?",
-                    reply_markup=markup,
-                )
-            case 10:
-                state.correct_user_answers_count = 0
-                bot.send_message(message.chat.id, "Ты ответил на 10 вопросов из 10 правильно. Ты мой лучший друг?")
+        bot.send_message(
+            chat_id,
+            f"Ты ответил на {state.correct_user_answers_count} вопросов из 10 правильно. {REZULT_END[state.correct_user_answers_count]}",
+            reply_markup=markup,
+        )
+        state.correct_user_answers_count = 0
 
-    def handler(self, message, bot, callback=None):
-        questions_and_answers = [
-            {"question": "Как меня зовут?", "answers": ["Егор", "Андрей", "Амид"]},
-            {"question": "Сколько мне лет?", "answers": ["13", "14", "15"]},
-            {"question": "В какой школе я учусь?", "answers": ["1158", "1488", "2007"]},
-            {"question": "Какую музыку я люблю?", "answers": ["Металл", "Пост-панк", "Панк"]},
-            {
-                "question": "Какими видами спорта я занимался?",
-                "answers": ["Плававнье и футбол", "Хоккей и футбол", "Скалолазанье и футбол"],
-            },
-            {"question": "На каком языке программирования я пишу?", "answers": ["Python", "C++", "Java"]},
-            {"question": "Какой вид одежды я хочу купить?", "answers": ["Рубашка", "Пальто", "Джинсовка"]},
-            {"question": "Какой мой любимый цвет?", "answers": ["Синий", "Красный", "Зеленый"]},
-            {"question": "Какие аниме я посмотрел?", "answers": ["Наруто", "Адский рай", "Убийца гоблинов"]},
-            {
-                "question": "Моя любимая игра? (Есть и другие игры, которые мне тоже очень сильно нравятся)",
-                "answers": ["Sanabi", "The forest", "Stardew Valley"],
-            },
-        ]
-        if callback is None:
-            state = self._users.setdefault(message.from_user.id, State(question_number=0, correct_user_answers_count=0))
-        else:
-            state = self._users[callback.from_user.id]
-
-        answers = questions_and_answers[state.question_number]["answers"]
-        question = questions_and_answers[state.question_number]["question"]
+    def show_question(self, bot, chat_id, question_number):
+        answers = QUESTIONS_AND_ANSWERS[question_number].answers
+        question = QUESTIONS_AND_ANSWERS[question_number].question
         markup = telebot.types.InlineKeyboardMarkup()
         btn1 = telebot.types.InlineKeyboardButton(answers[0], callback_data="qz/ответ1")
         btn2 = telebot.types.InlineKeyboardButton(answers[1], callback_data="qz/ответ2")
@@ -127,7 +81,11 @@ class Quiz:
         btn4 = telebot.types.InlineKeyboardButton("Пропустить", callback_data="qz/пропустить")
         markup.row(btn1, btn2)
         markup.row(btn3, btn4)
-        bot.send_message(message.chat.id, f"Вопрос {state.question_number + 1} из 10. {question}", reply_markup=markup)
+        bot.send_message(chat_id, f"Вопрос {question_number + 1} из 10. {question}", reply_markup=markup)
+
+    def handler(self, message, bot):
+        state = self._users.setdefault(message.from_user.id, State(question_number=0, correct_user_answers_count=0))
+        self.show_question(bot, message.chat.id, state.question_number)
 
     def responds_to_option(self, bot, callback, correct_answer, user_answer):
         state = self._users[callback.from_user.id]
@@ -141,28 +99,29 @@ class Quiz:
     def game_callback(self, callback, bot):
         state = self._users[callback.from_user.id]
 
-        correct_answers = [1, 2, 3, 2, 3, 1, 2, 3, 3, 1]
+        correct = QUESTIONS_AND_ANSWERS[state.question_number].correct
         match callback.data:
             case "qz/ответ1":
-                self.responds_to_option(bot, callback, correct_answers[state.question_number], 1)
+                self.responds_to_option(bot, callback, correct, 1)
             case "qz/ответ2":
-                self.responds_to_option(bot, callback, correct_answers[state.question_number], 2)
+                self.responds_to_option(bot, callback, correct, 2)
             case "qz/ответ3":
-                self.responds_to_option(bot, callback, correct_answers[state.question_number], 3)
+                self.responds_to_option(bot, callback, correct, 3)
 
         if state.question_number == 9:
             self.show_user_result(
-                callback.message,
                 bot,
+                callback.message.chat.id,
+                callback.from_user.id,
             )
             return
 
         state.question_number += 1
-        self.handler(callback.message, bot, callback=callback)
+        self.show_question(bot, callback.message.chat.id, state.question_number)
 
     def reply_game_callback(self, callback, bot):
         match callback.data:
             case "qzrpl/да":
-                self.handler(callback.message, bot, callback=callback)
+                self.handler(callback.message, bot)
             case "qzrpl/нет":
                 bot.send_message(callback.message.chat.id, "Хорошо. Спасибо за то, что сыграл")
